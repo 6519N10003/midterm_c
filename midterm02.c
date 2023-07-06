3323b135c9a3b79bf7ce760c8e2e1027208fd7c2
S6519N10003 <s6519n10003@sau.ac.th>
2023-07-06 13:53:39 +0700

    Version......1

---
+++ midterm02.c
@@ -0,0 +1,22 @@
+//git config --global user.name "S6519N10003"
+//git config --global user.email "s6519n10003@sau.ac.th"
+
+//git init
+//git add .
+//git commit -m "Version......"
+//git remote ...........
+//git push -u origin master
+
+//-------------------------------------------
+
+//git add .
+//git commit -m "Version......"
+//git push -u origin master
+
+#include <stdio.h>
+
+void main (){
+
+    
+    
+}
